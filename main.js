902f335b5715b64f2708c329206044505437106d
HossainRafi <hossainrafi71@gmail.com>
2022-11-17 13:17:34 +0600

    faq toggle script done

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+// FAQ answer show or hide
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+      faq.classList.toggle("open");
+      
+    //  Change toggle icon
+      const icon =faq.querySelector('.faq__icon i')
+      if (icon.className === "uil-angle-down") {
+        icon.className = "uil-angle-up";
+      } else {
+        icon.className = "uil-angle-down";
+      }
+  });
+});
